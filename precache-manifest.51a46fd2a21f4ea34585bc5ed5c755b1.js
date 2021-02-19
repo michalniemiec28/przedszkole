0dffca028a4ebcc8c145d11df16adba05dd29d37
Michał Niemiec <michal.niemiec@billennium.com>
2021-02-20 00:31:30 +0100

    first commit

---
+++ precache-manifest.51a46fd2a21f4ea34585bc5ed5c755b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800d0a42eb4d08c96b91fbffc3fe33da",
+    "url": "/przedszkole-nr-3/index.html"
+  },
+  {
+    "revision": "46e62c08d4aa0fdf3f12",
+    "url": "/przedszkole-nr-3/static/css/2.13dac556.chunk.css"
+  },
+  {
+    "revision": "d9c34da0f18fffc91237",
+    "url": "/przedszkole-nr-3/static/css/main.b357d8e4.chunk.css"
+  },
+  {
+    "revision": "46e62c08d4aa0fdf3f12",
+    "url": "/przedszkole-nr-3/static/js/2.d6f04b98.chunk.js"
+  },
+  {
+    "revision": "515b897eca4f8858e52d102cf03d08c7",
+    "url": "/przedszkole-nr-3/static/js/2.d6f04b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9c34da0f18fffc91237",
+    "url": "/przedszkole-nr-3/static/js/main.f94334dd.chunk.js"
+  },
+  {
+    "revision": "6b4b3a7b5b409bd77714",
+    "url": "/przedszkole-nr-3/static/js/runtime-main.5dcbbb59.js"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/przedszkole-nr-3/static/media/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "0a723e0c71368d7984a6a20aace59c16",
+    "url": "/przedszkole-nr-3/static/media/banner.0a723e0c.svg"
+  },
+  {
+    "revision": "334edff6a4b6b03de7268339e2cda38f",
+    "url": "/przedszkole-nr-3/static/media/hello.334edff6.svg"
+  },
+  {
+    "revision": "d33072d4a0c60241b31637a137718069",
+    "url": "/przedszkole-nr-3/static/media/logo.d33072d4.svg"
+  }
+]);
